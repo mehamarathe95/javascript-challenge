a0e60d972d94b852febc67a101675ba58ed4263f
Meha Marathe <mehamarathe@mehas-air.home>
2020-09-28 22:39:47 -0700

    function commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,3 +5,17 @@ var tableData = data;
 // Reference the table body
 var tbody = d3.select("tbody");
 
+// Write function to show UFO sightings 
+function UFOdata(Sightings) {
+    var tbody = d3.select("tbody");
+    Sightings.forEach(UFOInfo => {
+        var row = tbody.append("tr");
+        Object.entries(UFOInfo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.html(value);
+        });
+    });
+};
+
+UFOdata(tableData)
+
